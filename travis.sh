645d6830e01be2354d8e1985b19ec5716ee78d7a
Andre Torgal <andre.torgal@gmail.com>
2015-04-01 16:43:06 +0100

    chore(build): update bower to 1.4.1 and remove systemcheck from travis script

--- travis.sh
+++ travis.sh
@@ -3,13 +3,5 @@
 # install global dependencies
 npm install -g grunt-cli bower
 
-# checks node, npm, bower & grunt-cli version
-./bin/systemcheck.sh
-check=$?
-if [[ $check > 0 ]]
-then
-    exit $check;
-fi
-
 # ci tasks
 bower update && grunt ci
